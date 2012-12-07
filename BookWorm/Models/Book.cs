0e1054cc9c71d483454d56743dd5bd6f46aa2fed
Justin Wyer <justin@lifeasageek.com>
2012-12-07 13:21:44 +0200

    [#7] Rouan : Added all book properties

--- BookWorm/Models/Book.cs
+++ BookWorm/Models/Book.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookWorm.Models
 {
@@ -7,11 +8,38 @@ namespace BookWorm.Models
         [Required]
         public string Title { get; set; }
 
-        public string SubTitle { get; set; }
+        public string Subtitle { get; set; }
 
         [Required]
         public string Author { get; set; }
 
         public string Illustrator { get; set; }
+
+        [Required]
+        public string Editor { get; set; }
+
+        [Required]
+        public string Isbn { get; set;}
+
+        [Required]
+        public string Publisher { get; set; }
+
+        [Required]
+        public string Language { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [DisplayName("Recommended Age Group")]
+        [Required]
+        public string RecommendedAgeGroup { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [DisplayName("Cover Image")]
+        public string CoverImageUrl { get; set; }
+
+        public string Description { get; set; }
     }
 }
