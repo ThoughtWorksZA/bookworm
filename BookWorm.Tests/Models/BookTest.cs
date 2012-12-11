cb3e9d76585750274d3f6113fff3cc3ad20282c8
Justin Wyer <justin@lifeasageek.com>
2012-12-11 14:56:07 +0200

    [#7] Suparna/Kyle : adding test to check that required annotation is present for mandatory fields

--- BookWorm.Tests/Models/BookTest.cs
+++ BookWorm.Tests/Models/BookTest.cs
@@ -13,15 +13,45 @@ namespace BookWorm.Tests.Models
     public class BookTest
     {
         [TestMethod]
-        public void BookValidationsShouldBePresent()
+        public void LanguageShouldBeInValidLanguages()
         {
+            var bookClass = typeof (Book);
+            var propertyName = "Language";
+            var namedProperty = bookClass.GetProperty(propertyName);
+            Assert.IsNotNull(namedProperty);
+            Assert.AreEqual(1, namedProperty.GetCustomAttributes(typeof (ValidLanguage), false).Count(),
+                            "The Language property of the Book Object should have a ValidLanguage annotation.");
+        }
+
+        [TestMethod]
+        public void MandatoryPropertiesShouldBePresent()
+        {
+            var mandatoryProperties = new List<string>
+                {
+                    "Title",
+                    "Author",
+                    "Editor",
+                    "Isbn",
+                    "Publisher",
+                    "Language",
+                    "Genre",
+                    "RecommendedAgeGroup",
+                    "Country"
+                };
 
             var bookClass = typeof (Book);
-            var languageProperty = bookClass.GetProperty("Language");
-            Assert.IsNotNull(languageProperty);
-            Assert.AreEqual(1, languageProperty.GetCustomAttributes(typeof(ValidLanguage), false).Count(), "The Language property of the Book Object should have a ValidLanguage annotation.");
 
-
+            foreach (var propertyName in mandatoryProperties)
+            {
+                var namedProperty = bookClass.GetProperty(propertyName);
+                Assert.IsNotNull(namedProperty);
+                Assert.AreEqual(1,
+                                namedProperty.GetCustomAttributes(
+                                    typeof (System.ComponentModel.DataAnnotations.RequiredAttribute), false).Count(),
+                                string.Format(
+                                    "{0} is a mandatory field, but 'Required' annotation was not found on the Book object.",
+                                    propertyName));
+            }
         }
     }
 }
