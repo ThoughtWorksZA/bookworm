047993ac4abfd30475fe1c5c7f3b758bad03b901
Justin Wyer <justin@lifeasageek.com>
2012-12-11 13:42:09 +0200

    [#7] Suparna/Kyle : adding custom validator for language

---
+++ BookWorm.Tests/Models/Validations/ValidLanguageTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BookWorm.Models.Validations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookWorm.Tests.Models.Validations
+{
+    [TestClass]
+    public class ValidLanguageTest
+    {
+        [TestMethod]
+        public void ShouldBeValidWhenSpecifiedLanguageIsZulu()
+        {
+
+            var validator = new ValidLanguage();
+
+            Assert.IsTrue(validator.IsValid("Zulu"));
+        }
+    
+        [TestMethod]
+        public void ShouldBeInvalidWhenSpecifiedLanguageIsFrench()
+        {
+
+            var validator = new ValidLanguage();
+
+            Assert.IsFalse(validator.IsValid("French"));
+        }
+    }
+}
---
+++ BookWorm/Models/Validations/ValidLanguage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookWorm.Models.Validations
+{
+    public class ValidLanguage : ValidationAttribute //, IClientValidatable
+    {
+        public List<string> _validLanguages = new List<string>
+            {
+                "Afrikaans",
+                "English",
+                "Ndebele",
+                "Northern Sotho",
+                "Sotho",
+                "Swazi",
+                "Tswana",
+                "Tsonga",
+                "Venda",
+                "Xhosa",
+                "Zulu"
+            };
+
+        public override bool IsValid(object specifiedLanguage)
+        {
+            return checkSpecifiedLanguageIsValid(specifiedLanguage);
+        }
+
+        private bool checkSpecifiedLanguageIsValid(object specifiedLanguage)
+        {
+            return _validLanguages.Contains(specifiedLanguage);
+        }
+   
+    }
+}
