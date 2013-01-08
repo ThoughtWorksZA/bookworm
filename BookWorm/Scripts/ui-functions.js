c63a5e10dcab63f757b77af1631391c6b127a98f
Craig Wattrus <cwattrus@gmail.com>
2013-01-08 09:43:29 +0200

    Craig: Unified list styles for both pages and books. Simplified some messy CSS

--- BookWorm/Scripts/ui-functions.js
+++ BookWorm/Scripts/ui-functions.js
@@ -2,12 +2,12 @@
 
 function toggleDiscoveryMenu() {
     if (menuOpen) {
-        $("#discovery_menu").hide(300);
+        $("#discovery_menu").hide(200);
         $("#discovery_menu_toggle").html("Find a book <i class='icon-chevron-down'></i>");
         menuOpen = false;
     }
     else {
-        $("#discovery_menu").show(300);
+        $("#discovery_menu").show(200);
         $("#discovery_menu_toggle").html("Close <i class='icon-chevron-up'></i>");
         $("#searchQuery").focus();
         menuOpen = true;
