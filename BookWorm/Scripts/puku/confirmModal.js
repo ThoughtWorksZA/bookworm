336ad27f1c9af87b4c1788c85c13f1feb482f2ee
Craig Wattrus <cwattrus@gmail.com>
2013-01-08 12:12:46 +0200

    Craig: Changed working on modal title

--- BookWorm/Scripts/puku/confirmModal.js
+++ BookWorm/Scripts/puku/confirmModal.js
@@ -2,7 +2,7 @@
     $('input[data-confirm]').click(function () {
         var self = this;
         if (!$('#dataConfirmModal').length) {
-            $('body').append('<div id="dataConfirmModal" class="modal" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="dataConfirmLabel">Please Confirm</h3></div><div class="modal-body"></div><div class="modal-footer"><button class="btn btn-primary" id="dataConfirmOK">OK</button><button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button></div></div>');
+            $('body').append('<div id="dataConfirmModal" class="modal hide fade" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="dataConfirmLabel">Are you sure?</h3></div><div class="modal-body"></div><div class="modal-footer"><button class="btn btn-primary" id="dataConfirmOK">OK</button><button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button></div></div>');
         }
         $('#dataConfirmModal').find('.modal-body').text($(this).attr('data-confirm'));
         $('#dataConfirmOK').click(function () { self.form.submit(); });
