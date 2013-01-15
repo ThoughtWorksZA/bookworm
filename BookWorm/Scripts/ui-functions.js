bcea12467673f85c8e50d99ca4b2107b72c38c0e
Craig Wattrus <cwattrus@gmail.com>
2013-01-15 19:43:39 +0200

    Craig: Started experimenting with the top area to make it more friendly. Chnaged the admin tools section of book given the new post functionlity. Styled posts on book

--- BookWorm/Scripts/ui-functions.js
+++ BookWorm/Scripts/ui-functions.js
@@ -2,19 +2,15 @@
 
 function toggleDiscoveryMenu() {
     if (menuOpen) {
-        $("#discovery_menu").hide(200);
+        $("#discovery_menu").fadeOut(200);
         $("#discovery_menu_toggle").html("Find a book <i class='icon-chevron-down'></i>");
         menuOpen = false;
     }
     else {
-        $("#discovery_menu").show(200);
+        $("#discovery_menu").fadeIn(200);
         $("#discovery_menu_toggle").html("Close <i class='icon-chevron-up'></i>");
         $("#searchQuery").focus();
         menuOpen = true;
     }
 
-}
-
-function showLoginItems() {
- $('#login-items').popover('show');
 }
