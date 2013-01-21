cff4f4f0acd78bda71ea5abfec60d19e3ea363d5
Craig Wattrus <cwattrus@gmail.com>
2013-01-21 15:58:49 +0200

    Craig: Increased limit on the generic post title to 50 chars

--- BookWorm/Models/Post.cs
+++ BookWorm/Models/Post.cs
@@ -5,7 +5,7 @@ namespace BookWorm.Models
     public class Post : Model
     {
         [Required]
-        [StringLength(20, ErrorMessage = "{0} must be between {2} and {1} characters long", MinimumLength = 3)]
+        [StringLength(50, ErrorMessage = "{0} must be between {2} and {1} characters long", MinimumLength = 3)]
         public string Title { get; set; }
         [Required]
         public string Content { get; set; }
