c1e5f8fc0f6b58bada5d92b54f2e4adbf7664ab8
Ruimin Zhang <rmzhang@thoughtworks.com>
2013-07-18 10:50:41 +0200

    1.add js patch for ios hover. 2. use the relative path of ~

---
+++ BookWorm/Scripts/puku/iosPatch.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function() {
+    if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i))) {
+        $(".book-image").hover(function() {
+            var closest = $(this).closest("a");
+            if (closest && closest.attr("href")) {
+                window.location.href = closest.attr("href");
+            }
+        });
+    }
+});
