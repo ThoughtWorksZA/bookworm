2abe5db6891f0558f5e6d15c8ef5ec97ed71c65d
akanim <akanim@gmail.com>
2013-08-17 23:03:26 +0200

    Display drafts of the current user only

--- BookWorm.Tests/Controllers/Integration/PostsControllerTest.cs
+++ BookWorm.Tests/Controllers/Integration/PostsControllerTest.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using BookWorm.Controllers;
 using BookWorm.Models;
 using BookWorm.ViewModels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace BookWorm.Tests.Controllers.Integration
 {
@@ -30,6 +33,43 @@ namespace BookWorm.Tests.Controllers.Integration
                 WaitForTheLastWrite<Post>(session);
                 var controller = new PostsController(new Repository(session));
                 var result = (ViewResult)controller.News();
+                var pages = (PagedList.IPagedList<IBasePostInformation>)result.Model;
+
+                Assert.AreEqual(2, pages.Count);
+            });
+        }
+
+        [TestMethod]
+        public void ShouldDisplayOnlyDraftNewsCreatedByCurrentUserWhenListingDraftNews()
+        {
+            string userName = "user";
+
+            UsingSession((session) =>
+            {
+                var repository = new Repository(session);
+                var post = new Post { Id = 1, Title = "Post 1", Content = "A Post", UpdatedAt = DateTime.Now.AddMinutes(-1), IsDraft = true, Creator = userName };
+                repository.Create(post);
+                post = new Post { Id = 2, Title = "Post 2", Content = "A Post", UpdatedAt = DateTime.Now.AddMinutes(-1), IsDraft = true, Creator = userName };
+                repository.Create(post);
+                post = new Post { Id = 3, Title = "Post 3", Content = "A Post", UpdatedAt = DateTime.Now.AddMinutes(-1), IsDraft = true, Creator = "anotherUser" };
+                repository.Create(post);
+                var controller = new PostsController(repository);
+            });
+
+            UsingSession((session) =>
+            {
+                WaitForTheLastWrite<Post>(session);
+
+                var httpContextStub = new Mock<HttpContextBase>
+                {
+                    DefaultValue = DefaultValue.Mock
+                };
+                var user = Mock.Get(httpContextStub.Object.User);
+                user.Setup(u => u.Identity.Name).Returns(userName);
+
+                var controller = new PostsController(new Repository(session));
+                controller.ControllerContext = new ControllerContext(httpContextStub.Object, new RouteData(), controller);
+                var result = (ViewResult)controller.News(excludeDraft: false);
                 var pages = (PagedList.IPagedList<IBasePostInformation>)result.Model;
 
                 Assert.AreEqual(2, pages.Count);
--- BookWorm/Controllers/PostsController.cs
+++ BookWorm/Controllers/PostsController.cs
@@ -22,14 +22,24 @@ namespace BookWorm.Controllers
         public ActionResult News(int page = 1, int perPage = 9, bool excludeDraft = true)
         {
             var isDraft = !excludeDraft;
-            var posts = _repository.List<Post>().Where(p => p.IsDraft == isDraft).Select(post => new PostInformation(post));
+            var eventsAndNews = GetEventsAndNews(isDraft);
             var books = _repository.List<Book>();
             var bookPosts = books.SelectMany(x => x.Posts.Where(bp => bp.Type == BookPost.BookPostType.News || bp.Type == BookPost.BookPostType.Events).Select(y => new BookPostInformation(x.Id, y, x))).ToList();
             var allPosts = new List<IBasePostInformation>();
-            allPosts.AddRange(posts);
+            allPosts.AddRange(eventsAndNews);
             allPosts.AddRange(bookPosts);
             allPosts = allPosts.OrderByDescending(x => x.Model.UpdatedAt).ToList();
             return View(allPosts.ToPagedList(page, perPage));
+        }
+
+        private IEnumerable<PostInformation> GetEventsAndNews(bool isDraft)
+        {
+            if (!isDraft)
+                    return _repository.List<Post>().Where(p => p.IsDraft == isDraft).Select(post => new PostInformation(post));
+            
+            return _repository.List<Post>()
+                              .Where(p => p.IsDraft == isDraft && p.Creator.Equals(User.Identity.Name))
+                              .Select(post => new PostInformation(post));
         }
     }
 }
