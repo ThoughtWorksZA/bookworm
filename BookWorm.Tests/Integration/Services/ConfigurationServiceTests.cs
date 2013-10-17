49ba6845e0e5bfb2d851103bfcb74d48817c09f7
rouanw <rouanw@gmail.com>
2013-10-17 09:43:34 +0200

    Added configuration service to read email credentials from config file

---
+++ BookWorm.Tests/Integration/Services/ConfigurationServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using BookWorm.Services;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookWorm.Tests.Integration.Services
+{
+    [TestClass]
+    public class ConfigurationServiceTests
+    {
+        [TestMethod]
+        public void ShouldFetchEmailSenderAddressFromWebConfig()
+        {
+            var configService = new ConfigurationService();
+            configService.GetEmailSenderAddress().Should().Be("test-address-value");
+        }
+        
+        [TestMethod]
+        public void ShouldFetchEmailSenderPasswordFromWebConfig()
+        {
+            var configService = new ConfigurationService();
+            configService.GetEmailSenderPassword().Should().Be("test-password-value");
+        }
+    }
+}
---
+++ BookWorm/Services/ConfigurationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Configuration;
+
+namespace BookWorm.Services
+{
+    public class ConfigurationService
+    {
+        public string GetEmailSenderAddress()
+        {
+            return ConfigurationManager.AppSettings.Get("EmailSenderAddress");
+        }
+
+        public string GetEmailSenderPassword()
+        {
+            return ConfigurationManager.AppSettings.Get("EmailSenderPassword");
+        }
+    }
+}
