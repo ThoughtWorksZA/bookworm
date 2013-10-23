5fdd9c0e2aa6427c977abf9bef1236eacf7a18c4
rouanw <rouanw@gmail.com>
2013-10-23 09:10:31 +0200

    Put back the default BooksController constructor

--- BookWorm/Controllers/BooksController.cs
+++ BookWorm/Controllers/BooksController.cs
@@ -19,7 +19,12 @@ namespace BookWorm.Controllers
         private const string ProblemsSavingBookMessage = "There were problems saving this book";
 
         private readonly IFullTextSearch _fullTextSearch;
-        
+
+        public BooksController()
+        {
+            
+        }
+
         public BooksController(IFullTextSearch fullTextSearch = null)
         {
             _fullTextSearch = fullTextSearch ?? new FullTextSearchService(DocumentSession);
