bc5e5c9ed2158ca975fdd222ec8b491ca96955c1
rouanw <rouanw@gmail.com>
2013-12-20 14:18:06 +0200

    added first test for BookInformation

---
+++ BookWorm.Tests/ViewModels/BookInfromationTests.cs
@@ -0,0 +1,24 @@
+﻿using BookWorm.Models;
+using BookWorm.ViewModels;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookWorm.Tests.ViewModels
+{
+    [TestClass]
+    public class BookInfromationTests
+    {
+        [TestMethod]
+        public void SummaryShouldReturnBookDescriptionIfItsNull()
+        {
+            var bookInformation = new BookInformation
+            {
+                Model = new Book
+                {
+                    Description = null
+                }
+            };
+            bookInformation.Summary(150).Should().Be(null);
+        }
+    }
+}
